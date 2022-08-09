606b41a68719db644ea032751366608139cb146b
Daniel Nau <71359236+dannauu@users.noreply.github.com>
2022-08-09 13:38:41 -0500

    fixing gh deployment

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Gallery from './components/Gallery/Gallery'
 import Menu from './components/Menu/Menu'
 import OrderOnline from './components/OrderOnline/OrderOnline';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="gallery" element={<Gallery />} />
       <Route path="menu" element={<Menu />} />
       <Route path="order-online" element={<OrderOnline />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
