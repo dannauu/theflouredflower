f67d663687a147f699dff52689e76c041100a9a5
Daniel Nau <71359236+dannauu@users.noreply.github.com>
2022-08-10 12:27:59 -0500

    fixed a tags to link and hrefs to to

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ function App() {
     <>
       <nav className="bg-white border-purple-400 px-2 sm:px-4 py-2.5 border-b-4 navbar">
         <div className="container flex flex-wrap justify-between items-center mx-auto">
-          <Link to="/" className="flex items-center">
+          <Link to="/theflouredflower" className="flex items-center">
             <span className="self-center text-4xl font-semibold whitespace-nowrap logo text-purple-600 hover:text-purple-400 logo image">The Floured Flour</span>
           </Link>
           <div className="w-full md:block md:w-auto centerContent" id="mobile-menu">
             <ul className="flex navbar flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
               <li>
-                <Link to="/" className="block text-3xl py-2 mr-6 pl-33 md:p-0 text-purple-800 homeButton"><BiHomeHeart /></Link>
+                <Link to="/theflouredflower" className="block text-3xl py-2 mr-6 pl-33 md:p-0 text-purple-800 homeButton"><BiHomeHeart /></Link>
               </li>
               <li>
                 <Link to="/menu" className="block text-2xl py-2 pr-4 pl-33 border-b border-gray-100 hover:bg-gray-50 md:p-0 text-purple-800 ffLobsterTwo hover:text-purple-400">Menu</Link>
