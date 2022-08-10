b3e6b6475b75a37ac7664c43bd7d48469ff77d0f
Daniel Nau <71359236+dannauu@users.noreply.github.com>
2022-08-10 08:37:38 -0500

    fixed typo

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Gallery from './components/Gallery/Gallery'
 import Menu from './components/Menu/Menu'
 import OrderOnline from './components/OrderOnline/OrderOnline';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <Router basename='/theflouredflower'>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="gallery" element={<Gallery />} />
       <Route path="menu" element={<Menu />} />
       <Route path="order-online" element={<OrderOnline />} />
     </Routes>
-  </BrowserRouter>
+  </Router>
 );
