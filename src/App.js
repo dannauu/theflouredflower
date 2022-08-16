295e835d3ac206403c3571f6a7fb0453db953c22
Daniel Nau <71359236+dannauu@users.noreply.github.com>
2022-08-16 11:53:42 -0500

    added padding to menu

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           <div className="w-full md:block md:w-auto centerContent" id="mobile-menu">
             <ul className="flex navbar flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
               <a href="#menu" className="block text-2xl py-2 pr-4 pl-33 border-gray-100 hover:bg-gray-50 md:p-0 text-purple-800 ffLobsterTwo hover:text-purple-400">Menu</a>
-              <a href="#order-online" className="block text-2xl py-2 pr-4 pl-33 border-gray-100 hover:bg-gray-50 md:p-0 text-purple-800 ffLobsterTwo hover:text-purple-400">Order Online</a>
+              <a href="#order-online" className="block text-2xl py-2 pr-3 pl-33 border-gray-100 hover:bg-gray-50 md:p-0 text-purple-800 ffLobsterTwo hover:text-purple-400">Order Online</a>
               <a href="#gallery" className="block text-2xl py-2 pr-4 pl-3 border-gray-100 hover:bg-gray-50 md:p-0 text-purple-800 ffLobsterTwo hover:text-purple-400">Gallery</a>
 
             </ul>
