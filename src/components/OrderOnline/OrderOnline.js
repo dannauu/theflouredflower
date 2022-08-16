499d5ad68f5cb6b5bd620b75f44884a8718b0a03
Daniel Nau <71359236+dannauu@users.noreply.github.com>
2022-08-16 09:50:47 -0500

    made date pull up number pad instead of keyboard on mobile, fixed side to side scroll with css

--- src/components/OrderOnline/OrderOnline.js
+++ src/components/OrderOnline/OrderOnline.js
@@ -110,7 +110,7 @@ const OrderOnline = () => {
               <label className='text-2xl text-purple-700 italic' id='bakeryItemFlavor'>{flavor}</label>
             </div>
             <div className="relative z-0 mb-6 w-full group">
-              <input name="event-date" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-purple-200 border-0 border-b-2 border-purple-600 rounded-lg appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="" />
+              <input type="tel" name="event-date" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-purple-200 border-0 border-b-2 border-purple-600 rounded-lg appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="" />
               <label className='text-2xl text-purple-700 italic'>Event Date: (MM/DD/YYYY)</label>
             </div>
             <button type="submit" className="text-white bg-purple-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mb-2" onClick={showSuccess}>Submit</button>
