094f0f3e0e594b1083268ef301bb3cbfe3e24209
Daniel Nau <71359236+dannauu@users.noreply.github.com>
2022-08-17 22:36:33 -0500

    updated herobanner btn name

--- src/components/HeroBanner/HeroBanner.js
+++ src/components/HeroBanner/HeroBanner.js
@@ -29,7 +29,7 @@ export default function Example() {
                     href="#gallery"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-black bg-fuchsia-200 hover:bg-fuchsia-800 md:py-4 md:text-lg md:px-10"
                   >
-                    Gallery
+                    Past Work
                   </a>
                 </div>
               </div>
