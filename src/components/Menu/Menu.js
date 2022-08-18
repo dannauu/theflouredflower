9c7863bceb115732c45b8552cbc6110ccb856673
Daniel Nau <71359236+dannauu@users.noreply.github.com>
2022-08-18 16:38:42 -0500

    made some menu changes

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -35,6 +35,15 @@ const Menu = () => {
           </li>
         ))}
       </ul>
+      <div className='text-purple-800 text-3xl text-center mt-7 ffLobsterTwo'>Sheet Cakes</div>
+      <ul className='cakesUL'>
+        {cakes.sheetCakes.map((item) => (
+          <li key={uniqid()} className='cakes-list-item'>
+            {item}
+          </li>
+        ))}
+      </ul>
+      <p className='text-purple-800 text-1xl text-center mt-3 italic font-bold pl-2 pr-2 pb-2'>Includes white icing with a border and personalized name or quote like "Happy Birthday"</p>
     </section>
     <section className='bg-purple-300 container m-auto rounded-xl mb-2'>
       <div className='text-purple-800 text-5xl text-center ffLobsterTwo pt-3 italic' id='cupcakes'>Cupcakes</div>
@@ -57,7 +66,7 @@ const Menu = () => {
       <div className='text-purple-800 text-3xl text-center mt-7 ffLobsterTwo' >Pricing</div>
       <p className='text-purple-800 text-1xl text-center mt-3 italic font-bold pl-2 pr-2'>Classic Cupcakes are $18.00/dozen which includes a classic flavor and a single color icing swirl.</p>
       <p className='text-purple-800 text-1xl text-center mt-3 italic font-bold pl-2 pr-2'>Premium Cupcakes are $21.00/dozen which includes a premium flavor and a single or multi colored icing swirl.</p>
-      <p className='text-purple-800 text-1xl text-center mt-3 italic font-bold pl-2 pr-2 pb-3'>Classic Cupcakes are $2.25/cupcake which includes a premium personal name, date, and any other information wanted. Also includes icing or fondant flavors. </p>
+      <p className='text-purple-800 text-1xl text-center mt-3 italic font-bold pl-2 pr-2 pb-3'>Decorative Cupcakes are $2.25/cupcake which includes a premium flower, personal name, date, and any other information wanted. Also includes icing or fondant flavors. </p>
     </section>
     <section className='bg-purple-200 container m-auto rounded-xl'>
       <div className='text-purple-800 text-5xl text-center ffLobsterTwo italic' id='cookies'>Cookies</div>
@@ -71,7 +80,16 @@ const Menu = () => {
       </ul>
       <div className='text-purple-800 text-3xl text-center mt-7 ffLobsterTwo'>Pricing</div>
       <p className='text-purple-800 text-1xl text-center mt-3 italic font-bold'>Classic cookies are all $9.00/dozen.</p>
-      <p className='text-purple-800 text-1xl text-center mt-3 italic font-bold pb-3'>Premium cookies all require a phone consultation to dictate pricing and design.</p>
+      <div className='text-purple-800 text-3xl text-center mt-7 ffLobsterTwo'>Decorative Sugar Cookies</div>
+      <div className='text-purple-800 text-2xl text-center mt-7 ffLobsterTwo'>Flavors</div>
+      <ul className='cakesUL'>
+        {cookies.premiumCookiesFlavors.map((item) => (
+          <li key={uniqid()} className='cakes-list-item'>
+            {item}
+          </li>
+        ))}
+      </ul>
+      <p className='text-purple-800 text-1xl text-center mt-3 italic font-bold pb-2'>These cookies are special ordered with royal icing designs. These cookies are more complex and require a phone consultation before a price can be quoted.</p>
     </section>
     </>
   )
--- src/menuItems.js
+++ src/menuItems.js
@@ -4,12 +4,16 @@ const cakes = {
     ],
     premiumFlavors: ['Espresso/Coffee', 'Pumpkin Spice', 'Apple Cider'],
     sizes: [
-        '6" ($25.00)', '8" ($35.00)', '10"  ($45.00)', '12"  ($55.00)'
+        '6" Round ($25.00)', '8" Round ($35.00)', '10"  Round ($45.00)', '12" Round ($55.00)'
+    ],
+    sheetCakes: [
+        '1/4 Sheet Cake (Serves 24-26) $50.00', '1/2 Sheet Cake (Serves 54) $75.00','Full Sheet Cake (Serves 120) $150.00'
     ]
 }
 
 const cookies = {
     flavors: ['Chocolate Chunk', 'Lemon Drop', 'Peanut Butter', 'M&M', 'Sugar (No Icing)'],
+    premiumCookiesFlavors: ['Vanilla', 'Almond', 'Lemon'],
 }
 
 const cupcakes = {
