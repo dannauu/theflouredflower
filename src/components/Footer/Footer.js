f73ff1c4f2b0a33b3daf94c3ce8c74530f20d879
Daniel Nau <71359236+dannauu@users.noreply.github.com>
2022-08-17 22:41:06 -0500

    testing netlify deployment for custom domain

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
 
     <footer className="p-4 bg-white border-t-4 border-purple-400 sm:p-6 flex justify-center">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-purple-500">© 2022 <a href="/" className="hover:underline">The Floured Flour ™</a>. All Rights Reserved.</span>
+          <span className="text-sm text-purple-500">© 2022 <a href="/" className="hover:underline">The Floured Flower ™</a>. All Rights Reserved.</span>
         </div>
     </footer>
 
