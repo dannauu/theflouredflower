10fdc703068b6e6a43c0e46aed1863c34e337f31
Daniel Nau <71359236+dannauu@users.noreply.github.com>
2022-11-01 11:14:29 -0500

    font sizing

--- src/components/HeroBanner/HeroBanner.js
+++ src/components/HeroBanner/HeroBanner.js
@@ -8,7 +8,7 @@ const HeroBanner = () => {
     <div class="hero-image">
       <div class="hero-text">
         <h1></h1>
-        <p className='italic text-xl pb-10'>The holidays are here! Want to enjoy these moments without the stress of baking until the early hours? Let me handle it for you. Click the holiday menu button below and order your sweet treats a week in advance. Kick your feet up, drink that coco and enjoy your Hallmark movies while I whip up some magic to help you this holiday season.  </p>
+        <p className='italic text-xl pb-10 textMobile'>The holidays are here! Want to enjoy these moments without the stress of baking until the early hours? Let me handle it for you. Click the holiday menu button below and order your sweet treats a week in advance. Kick your feet up, drink that coco and enjoy your Hallmark movies while I whip up some magic to help you this holiday season.  </p>
         <a href='#holidayMenu'><button type="button" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Holiday Menu</button></a>
       </div>
     </div>
