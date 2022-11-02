1572cf3e9c7021c9ad8052d765ac04d66d3f1523
Daniel Nau <71359236+dannauu@users.noreply.github.com>
2022-11-02 12:48:35 -0500

    fixed a couple typos

--- src/components/HolidayMenu/HolidayMenu.js
+++ src/components/HolidayMenu/HolidayMenu.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const HolidayMenu = () => {
     return (
-        <section className='container m-auto text-center rounded-xl holidayMenuBG text-white p-10 mt-2' id='holidayMenu'>
+        <section className='container m-auto text-center rounded-xl holidayMenuBG text-white p-10 mt-2 mb-2' id='holidayMenu'>
             <div className='p-1'>
-                <h1 className='mt-2 text-6xl italic christmasFont'>Holiday Menu</h1>
+                <h1 className='text-6xl italic christmasFont'>Holiday Menu</h1>
                 <h1 className='mt-5 text-3xl christmasFont'>Breads</h1>
                 <p className='text-sm'>Loaf of Braided Challa Bread (Circle or Rectangular) --- <span className='italic font-bold'>$15.00</span></p>
                 <p className='text-sm'>Pan Bread (6) White Rolls --- <span className='italic font-bold'>$7.00</span></p>
@@ -13,13 +13,13 @@ const HolidayMenu = () => {
                 <p className='text-sm'>Pumpkin --- <span className='italic font-bold'>$12.00</span></p>
                 <p className='text-sm'>Chocolate --- <span className='italic font-bold'>$12.00</span></p>
                 <p className='text-sm'>Pecan --- <span className='italic font-bold'>$12.00</span></p>
-                <p className='text-sm italic font-bold'>Any merangue added is an extra $3.00</p>
+                <p className='text-sm italic font-bold'>Any meringue added is an extra $3.00</p>
                 <h1 className='mt-5 text-3xl christmasFont'>Decorated Royal Icing Sugar Cookies</h1>
                 <p className='text-sm'>Set of 3 --- <span className='italic font-bold'>$6.00</span></p>
                 <p className='text-sm'>Set of 6 --- <span className='italic font-bold'>$12.00</span></p>
                 <p className='text-sm'>Set of 9 --- <span className='italic font-bold'>$18.00</span></p>
                 <p className='text-sm'>Set of 12 --- <span className='italic font-bold'>$24.00</span></p>
-                <p className='text-sm italic font-bold'>Predecorated Choices include Ornament, Christmas Tree, Candy Cane, Gingerbread Man, Santa, Reindeer, <br /> "Joy to the world, "Unto us a child is born", "Merry", "Joy", "Oh holy night", "Jesus is the reason"</p>
+                <p className='text-sm italic font-bold'>Predecorated choices include Ornament, Christmas Tree, Candy Cane, Gingerbread Man, Santa, Reindeer, <br /> "Joy to the world, "Unto us a child is born", "Merry", "Joy", "Oh holy night", "Jesus is the reason"</p>
                 <h1 className='mt-5 text-3xl christmasFont'>Sugar Cookie Kits</h1>
                 <p className='text-sm'>Kit #1 includes 3 cookie shapes with 3 different colors of your choice --- <span className='italic font-bold'>$9.00</span></p>
                 <p className='text-sm'>Kit #2 includes 4 cookie shapes with 4 different colors of your choice --- <span className='italic font-bold'>$12.00</span></p>
--- src/components/ThankYou/ThankYou.js
+++ src/components/ThankYou/ThankYou.js
@@ -5,11 +5,9 @@ import Gallery from '../Gallery/Gallery'
 const ThankYou = () => {
   return (
     <div className='text-center bg-rose-200 p-20 h-full'>
-        <h1 className='text-4xl'>Thank you for ordering, Kaylynn will be in contact with you in the next 2 business days</h1>
-        <h1 className='text-2xl mt-4 pb-10 italic'>Check your email for confirmation of your order</h1>
-        <BsFillBagCheckFill className='text-9xl m-auto'/>
-        <h1 className='text-2xl mt-10'>Check out my gallery below while you wait</h1>
-        <Gallery />
+        <h1 className='text-4xl textMobile'>Thank you for ordering, Kaylynn will be in contact with you in the next 2 business days</h1>
+        <h1 className='text-2xl mt-4 pb-10 italic textMobile2'>Check your email for confirmation of your order</h1>
+        <BsFillBagCheckFill className='text-9xl m-auto mobileLogo'/>
     </div>
 
   )
